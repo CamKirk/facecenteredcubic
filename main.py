e13c454987591d1f7a63e44d1177b18e12e0b2ed
camkirk <camdenkirkland@hotmail.com>
2018-12-01 14:34:09 -0600

    created a reminder comment

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import csv
 
 csv_loc = os.path.join("data", "info.csv")
-
+# hey this is a comment, remember to close this file!
 with open(csv_loc, mode="r") as csvfile:
     csvreader = csv.reader(csvfile,newLine="")
 
